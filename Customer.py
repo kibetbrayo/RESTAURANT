e66b5003eb7864b267b39d45c7b94712d036c1fd
kibetbrian <brian.kibet2@student.moringaschool.com>
2023-08-27 20:05:10 +0300

    customer.py file completed

---
+++ Customer.py
@@ -0,0 +1,40 @@
+class Customer:
+    all_customers = []
+
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+        self.reviews = []
+        Customer.all_customers.append(self)
+
+    def full_name(self):
+        return f"{self.given_name} {self.family_name}"
+
+    @classmethod
+    def all(cls):
+        return cls.all_customers
+
+    def restaurants(self):
+        reviewed_restaurants = []
+        for review in self.reviews:
+            if review.restaurant() not in reviewed_restaurants:
+                reviewed_restaurants.append(review.restaurant())
+        return reviewed_restaurants
+
+    def add_review(self, restaurant, rating):
+        review = Review(self, restaurant, rating)
+        self.reviews.append(review)
+
+    def num_reviews(self):
+        return len(self.reviews)
+
+    @classmethod
+    def find_by_name(cls, name):
+        for customer in cls.all_customers:
+            if customer.full_name() == name:
+                return customer
+        return None
+
+    @classmethod
+    def find_all_by_given_name(cls, name):
+        return [customer for customer in cls.all_customers if customer.given_name == name]
